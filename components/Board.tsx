15284a2dc2e8896bd6cf46e1f0bf928cf02ff70c
juanda <juandacorreo@gmail.com>
2021-02-10 23:52:15 +0100

    use react-pdf

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,58 +1,127 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import {
+  Page,
+  PDFViewer,
+  Image,
+  View,
+  Document,
+  StyleSheet,
+  PDFDownloadLink,
+} from '@react-pdf/renderer'
 import { Stage, Layer, Rect, Text, Line, Group } from 'react-konva'
 
 const padding = 40
 const height = 600
 const width = 3000
 
-const Board = (): JSX.Element => (
-  <Stage width={window.innerWidth} height={window.innerHeight}>
-    <Layer>
-      <Text text="Try click on rect" />
-      <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        color={'green'}
-        shadowBlur={5}
-      />
-    </Layer>
-    <Layer>
-      {[...Array(1000).keys()].map((i) => {
-        console.log(
-          Math.round(i * padding) + 0.5,
-          '****************************************************'
-        )
-        return (
-          <Group key={i}>
-            <Line
-              key={`line-vertical-${i}`}
-              points={[
-                Math.round(i * padding) + 0.5,
-                0,
-                Math.round(i * padding) + 0.5,
-                height,
-              ]}
-              tension={0.5}
-              stroke="#ddd"
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+  const data = b64Data.replace('data:image/png;base64,', '')
+  const byteCharacters = atob(data)
+  const byteArrays = []
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize)
+
+    const byteNumbers = new Array(slice.length)
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i)
+    }
+
+    const byteArray = new Uint8Array(byteNumbers)
+    byteArrays.push(byteArray)
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType })
+  return blob
+}
+class Board extends Component {
+  private stageRef
+  state = {
+    printView: false,
+  }
+  constructor(props: never) {
+    super(props)
+    this.stageRef = React.createRef()
+  }
+
+  handlePrint = (): void => this.setState({ printView: true })
+  render(): JSX.Element {
+    const { printView } = this.state
+    let canvasImg: HTMLInputElement
+    let MyDoc: JSX.Element
+    if (printView) {
+      const base64Code = this.stageRef.current.toDataURL()
+      console.log(base64Code)
+      const data = b64toBlob(base64Code, 'image/png')
+      canvasImg = <Image src={base64Code} />
+      MyDoc = (
+        <Document>
+          <Page>
+            <View>{canvasImg}</View>
+          </Page>
+        </Document>
+      )
+    }
+    return printView ? (
+      <PDFDownloadLink document={MyDoc} fileName="somename.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? 'Loading document...' : 'Download now!'
+        }
+      </PDFDownloadLink>
+    ) : (
+      <div>
+        <Button variant="contained" color="primary" onClick={this.handlePrint}>
+          Imprimir
+        </Button>
+        <Stage
+          ref={this.stageRef}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        >
+          <Layer>
+            <Text text="Try click on rect" />
+            <Rect
+              x={20}
+              y={20}
+              width={50}
+              height={50}
+              color={'green'}
+              shadowBlur={5}
             />
-            <Line
-              key={`line-horizontal-${i}`}
-              points={[
-                0,
-                Math.round(i * padding) + 0.5,
-                width,
-                Math.round(i * padding) + 0.5,
-              ]}
-              tension={0.5}
-              stroke="#ddd"
-            />
-          </Group>
-        )
-      })}
-    </Layer>
-  </Stage>
-)
+          </Layer>
+          <Layer>
+            {[...Array(1000).keys()].map((i) => (
+              <Group key={i}>
+                <Line
+                  key={`line-vertical-${i}`}
+                  points={[
+                    Math.round(i * padding) + 0.5,
+                    0,
+                    Math.round(i * padding) + 0.5,
+                    height,
+                  ]}
+                  tension={0.5}
+                  stroke="#ddd"
+                />
+                <Line
+                  key={`line-horizontal-${i}`}
+                  points={[
+                    0,
+                    Math.round(i * padding) + 0.5,
+                    width,
+                    Math.round(i * padding) + 0.5,
+                  ]}
+                  tension={0.5}
+                  stroke="#ddd"
+                />
+              </Group>
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+    )
+  }
+}
 
 export default Board
--- pages/boards.tsx
+++ pages/boards.tsx
@@ -1,18 +1,15 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement } from 'react'
 import dynamic from 'next/dynamic'
 
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('../components/Board'),
-  { ssr: false }
-)
+const BoardWithNoSSR = dynamic(() => import('../components/Board'), {
+  ssr: false,
+})
 
-const BoardPage: FC = (): ReactElement  => {
-
-
+const BoardPage: FC = (): ReactElement => {
   return (
     <div>
       <h1>Board Page</h1>
-      <DynamicComponentWithNoSSR />
+      <BoardWithNoSSR />
     </div>
   )
 }
