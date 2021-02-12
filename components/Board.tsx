a25a0f53db82390900ca9a300cf9da495b13818b
juanda <juandacorreo@gmail.com>
2021-02-12 11:23:55 +0100

    implementing sidebar

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import {
   Page,
-  PDFViewer,
   Image,
   View,
   Document,
@@ -11,54 +10,62 @@ import {
 } from '@react-pdf/renderer'
 import { Stage, Layer, Rect, Text, Line, Group } from 'react-konva'
 
-const padding = 40
-const height = 600
-const width = 3000
+const getHeight = (width: number) => (841.89 / 595.28) * width
 
-const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
-  const data = b64Data.replace('data:image/png;base64,', '')
-  const byteCharacters = atob(data)
-  const byteArrays = []
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize)
-
-    const byteNumbers = new Array(slice.length)
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i)
-    }
-
-    const byteArray = new Uint8Array(byteNumbers)
-    byteArrays.push(byteArray)
-  }
-
-  const blob = new Blob(byteArrays, { type: contentType })
-  return blob
-}
+const styles = StyleSheet.create({
+  image: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+})
 class Board extends Component {
   private stageRef
+
   state = {
     printView: false,
+    width: window.innerWidth,
+    height: getHeight(window.innerWidth),
+    zoom: 90,
   }
+
   constructor(props: never) {
     super(props)
     this.stageRef = React.createRef()
   }
 
+  handleWindowResize = (): void => {
+    this.setState({
+      width: window.innerWidth,
+      height: getHeight(window.innerWidth),
+    })
+  }
+
+  componentDidMount(): void {
+    window.addEventListener('resize', this.handleWindowResize)
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('resize', this.handleWindowResize)
+  }
+
   handlePrint = (): void => this.setState({ printView: true })
   render(): JSX.Element {
-    const { printView } = this.state
-    let canvasImg: HTMLInputElement
+    const { printView, width, height, zoom } = this.state
+    const zoomWidth = (width * zoom) / 100
+    const zoomHeight = (height * zoom) / 100
+    const padding = zoomWidth / 20
+    const horizontalLines = Math.trunc(zoomHeight / padding)
+    const verticalLines = Math.trunc(zoomWidth / padding)
     let MyDoc: JSX.Element
     if (printView) {
       const base64Code = this.stageRef.current.toDataURL()
-      console.log(base64Code)
-      const data = b64toBlob(base64Code, 'image/png')
-      canvasImg = <Image src={base64Code} />
       MyDoc = (
         <Document>
-          <Page>
-            <View>{canvasImg}</View>
+          <Page size="A4">
+            <View>
+              <Image src={base64Code} style={styles.img} />
+            </View>
           </Page>
         </Document>
       )
@@ -74,51 +81,57 @@ class Board extends Component {
         <Button variant="contained" color="primary" onClick={this.handlePrint}>
           Imprimir
         </Button>
-        <Stage
-          ref={this.stageRef}
-          width={window.innerWidth}
-          height={window.innerHeight}
+        <div
+          style={{
+            border: 2,
+            borderStyle: 'solid',
+            borderColor: '#ddd',
+            width: `${zoomWidth}px`,
+            margin: '0 auto',
+          }}
         >
-          <Layer>
-            <Text text="Try click on rect" />
-            <Rect
-              x={20}
-              y={20}
-              width={50}
-              height={50}
-              color={'green'}
-              shadowBlur={5}
-            />
-          </Layer>
-          <Layer>
-            {[...Array(1000).keys()].map((i) => (
-              <Group key={i}>
+          <Stage ref={this.stageRef} width={zoomWidth} height={zoomHeight}>
+            <Layer>
+              <Text text="Try click on rect" />
+              <Rect
+                x={20}
+                y={20}
+                width={50}
+                height={50}
+                color={'green'}
+                shadowBlur={5}
+              />
+            </Layer>
+            <Layer>
+              {[...Array(verticalLines).keys()].map((i) => (
                 <Line
                   key={`line-vertical-${i}`}
                   points={[
-                    Math.round(i * padding) + 0.5,
+                    Math.round((i + 1) * padding) + 0.5,
                     0,
-                    Math.round(i * padding) + 0.5,
+                    Math.round((i + 1) * padding) + 0.5,
                     height,
                   ]}
                   tension={0.5}
                   stroke="#ddd"
                 />
+              ))}
+              {[...Array(horizontalLines).keys()].map((i) => (
                 <Line
                   key={`line-horizontal-${i}`}
                   points={[
                     0,
-                    Math.round(i * padding) + 0.5,
+                    Math.round((i + 1) * padding) + 0.5,
                     width,
-                    Math.round(i * padding) + 0.5,
+                    Math.round((i + 1) * padding) + 0.5,
                   ]}
                   tension={0.5}
                   stroke="#ddd"
                 />
-              </Group>
-            ))}
-          </Layer>
-        </Stage>
+              ))}
+            </Layer>
+          </Stage>
+        </div>
       </div>
     )
   }
